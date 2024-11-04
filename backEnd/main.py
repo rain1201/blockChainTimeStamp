8798fc8be21a94fb88f224a832668361450c8453
rain1201 <rain1201@yeah.net>
2024-11-04 22:27:30 +0800

    add space into sql

--- backEnd/main.py
+++ backEnd/main.py
@@ -255,7 +255,7 @@ def querySingleRecords():
     cnt=0
     if(sessionId!="anonymous"):cnt=cursor.execute('SELECT recordId, fileHash, selfSign, txId, status, userId' 
     'FROM records WHERE userId=%s AND recordId="%s";',[userId,recordId])
-    else:cnt=cursor.execute('SELECT recordId, fileHash, selfSign, txId, status, userId' 
+    else:cnt=cursor.execute('SELECT recordId, fileHash, selfSign, txId, status, userId ' 
     'FROM records WHERE recordId="%s" AND (status=3 OR status=4);',[recordId])
     ret={"code":0,"count":cnt,"data":[]}
     for _ in range(cnt):ret["data"].append(cursor.fetchone())
