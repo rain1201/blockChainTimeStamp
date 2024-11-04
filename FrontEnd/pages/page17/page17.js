1fcf0bf3dfe7ce22a3626e991fa8c39dfbf67474
kang-siyou <2399258650@qq.com>
2024-11-04 22:34:22 +0800

    Add files via upload

---
+++ FrontEnd/pages/page17/page17.js
@@ -0,0 +1,9 @@
+ const login = document.querySelector('.text');
+            login.addEventListener('click', function () {
+                window.location.href = '../page2/page2.html';
+            });
+
+const Register = document.querySelector('.text_2');
+            Register.addEventListener('click', function () {
+                window.location.href = '../page1/page1.html';
+            });
